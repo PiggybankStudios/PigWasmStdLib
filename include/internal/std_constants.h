bcecd199f65b581f0efee34abb3d5e2a7c6321f5
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2023-10-16 18:33:00 -0700

    WASM_PAGE_SIZE should have been 64kB, not 4kB.

--- include/internal/std_constants.h
+++ include/internal/std_constants.h
@@ -7,7 +7,7 @@ Date:   10\07\2023
 #ifndef _STD_CONSTANTS_H
 #define _STD_CONSTANTS_H
 
-#define WASM_MEMORY_PAGE_SIZE      (4*1024)   //4kB or 4096 bytes
+#define WASM_MEMORY_PAGE_SIZE      (64*1024)   //64kB or 65,536 bytes
 #define WASM_MEMORY_MAX_NUM_PAGES  (512*1024) //2GB or 524,288 pages
 
 #endif //  _STD_CONSTANTS_H
